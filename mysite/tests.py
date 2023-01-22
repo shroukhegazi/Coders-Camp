0ebcca0dd2ccc73df5c8afd20a599a065ed5ae28
shroukhegazi <shroukhegazi99@gmail.com>
2023-01-22 23:35:33 +0200

    Test Post, Comment views

--- mysite/tests.py
+++ mysite/tests.py
@@ -1,3 +1,224 @@
-from django.test import TestCase
+from django.urls import reverse 
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from mysite.models import Post, CustomUser, Comment
 
-# Create your tests here.
+class TestPostViews(APITestCase):
+#Creating Post Tests
+    
+    def test_post_by_unauthenicated_user(self):
+        client = APIClient()
+        data = {
+            "title": "CodeCamp",
+            "url":"https://www.django-rest-framework.org/"
+        }
+        response = client.post("/post/",data)
+        res_data = response.data
+        assert response.status_code == 401
+
+    def test_post_by_authenicated_user(self):
+        client = APIClient()
+
+        user=CustomUser.objects.create_user("testing1", "134679//t")
+        client.force_authenticate(user=user)
+        data = {
+            "title": "CodeCamp",
+            "url":"https://www.django-rest-framework.org/"
+        }
+
+        response = client.post("/post/",data)
+        res_data = response.data["data"]
+        assert response.status_code == 200
+        assert res_data["title"] == data["title"]
+        assert res_data["url"] == data["url"]
+        post = Post.objects.filter(title="CodeCamp").first()
+        assert post is not None
+        assert post.url == "https://www.django-rest-framework.org/"
+
+    def test_post_by_authenicated_user_missing_url(self):
+        client = APIClient()
+        user=CustomUser.objects.create_user("testing1", "134679//t")
+        client.force_authenticate(user=user)
+
+        data = {
+            "title": "CodeCamp"
+        }
+
+        response = client.post("/post/",data)
+        res_data = response.data["data"]
+        assert response.status_code == 400
+
+    def test_post_by_authenicated_user_missing_title(self):
+        client = APIClient()
+
+        user=CustomUser.objects.create_user("testing1", "134679//t")
+        client.force_authenticate(user=user)
+        data = {
+           "url":"https://www.django-rest-framework.org/"
+        }
+
+        response = client.post("/post/",data)
+        res_data = response.data["data"]
+        assert response.status_code == 400
+
+    def test_post_by_authenicated_user_without_data(self):
+        client = APIClient()
+
+        user=CustomUser.objects.create_user("testing1", "134679//t")
+        client.force_authenticate(user=user)
+
+        response = client.post("/post/",{})
+        res_data = response.data["data"]
+        assert response.status_code == 400        
+
+    def test_partial_update_post(self):
+        client = APIClient()
+
+        user=CustomUser.objects.create_user("testing1", "134679//t")
+        client.force_authenticate(user=user)
+        post= Post(title="CodeCamp",url="https://www.django-rest-framework.org/", user=user)
+        post.save()
+        response = client.put("/post/"+str(post.pk)+"/" , {"title":"coding"})
+        #Hard Refresh The Object From The DB
+        post = Post.objects.get(id=post.pk)
+        assert response.status_code == 200
+        assert post.title == "coding"  
+
+    def test_update_unexist_post(self):
+        client = APIClient()
+        user=CustomUser.objects.create_user("testing1", "134679//t")
+        client.force_authenticate(user=user)
+        response = client.delete("/post/60/",{"title":"dosent exist"})
+        assert response.status_code == 404      
+#Get Posts Tests
+    def test_get_by_unauthenicated_user(self):
+        client = APIClient()
+        post= Post(title="CodeCamp",url="https://www.django-rest-framework.org/")
+        post.save()
+        response = client.get("/post/"+str(post.pk)+"/")
+        assert response.status_code == 401       
+
+    def test_get_post(self):
+        client = APIClient()
+
+        user=CustomUser.objects.create_user("testing1", "134679//t")
+        client.force_authenticate(user=user)
+
+        post= Post(title="CodeCamp",url="https://www.django-rest-framework.org/", user=user)
+        post.save()
+        response = client.get("/post/"+str(post.pk)+"/")
+        assert response.status_code == 200
+        assert post.title == response.data["title"]  
+
+    def test_get_unexist_post(self):
+        client = APIClient()
+        user=CustomUser.objects.create_user("testing1", "134679//t")
+        client.force_authenticate(user=user)
+        response = client.get("/post/60/")
+        assert response.status_code == 404    
+
+#Delete Post Tests
+    def test_delete_by_unauthenicated_user(self):
+        client = APIClient()
+        post= Post(title="CodeCamp",url="https://www.django-rest-framework.org/")
+        post.save()
+        response = client.delete("/post/"+str(post.pk)+"/")
+        assert response.status_code == 401        
+
+    def test_delete_post(self):
+        client = APIClient()
+        user=CustomUser.objects.create_user("testing1", "134679//t")
+        client.force_authenticate(user=user)
+
+        post= Post(title="CodeCamp",url="https://www.django-rest-framework.org/", user=user)
+        post.save()
+        response = client.delete("/post/"+str(post.pk)+"/")
+        assert response.status_code == 200
+
+    def test_delete_unexist_post(self):
+        client = APIClient()
+        user=CustomUser.objects.create_user("testing1", "134679//t")
+        client.force_authenticate(user=user)
+        response = client.delete("/post/60/")
+        assert response.status_code == 404
+
+class TestCommentViews(APITestCase):
+#Creating comments tests
+    def test_comment_by_authenicated_user(self):
+        client = APIClient()
+        user=CustomUser.objects.create_user("testing1", "134679//t")
+        client.force_authenticate(user=user)
+        post= Post(title="CodeCamp",url="https://www.django-rest-framework.org/", user=user)
+        post.save()
+        data= {
+            "comment":"My Comment"
+        }
+        response = client.post("/"+str(post.pk)+"/comment/",data)
+        res_data = response.data
+        assert response.status_code == 201
+        assert res_data["comment"] == data["comment"]
+        comment = Comment.objects.filter(comment="My Comment").first()
+        assert comment is not None
+
+
+    def test_comment_by_unauthenicated_user(self):
+        client = APIClient()
+        user=CustomUser.objects.create_user("testing1", "134679//t")
+        post= Post(title="CodeCamp",url="https://www.django-rest-framework.org/", user=user)
+        post.save()
+        data= {
+            "comment":"My Comment"
+        }
+        response = client.post("/"+str(post.pk)+"/comment/",data)
+        assert response.status_code == 401
+      
+#Getting comments tests 
+    def test_get_exist_comment(self):
+        client = APIClient()
+        user=CustomUser.objects.create_user("testing1", "134679//t")
+        client.force_authenticate(user=user)
+
+        post= Post(title="CodeCamp",url="https://www.django-rest-framework.org/", user=user)
+        post.save()
+        comment= Comment(comment="My Comment", user=user, post=post)
+        comment.save()
+    
+        response = client.get("/"+str(post.pk)+"/comment/"+str(comment.pk))
+        res_data = response.data
+        assert response.status_code == 200
+        assert res_data["comment"] == comment.comment
+
+
+    def test_get_unexist_comment(self):
+        client = APIClient()
+        user=CustomUser.objects.create_user("testing1", "134679//t")
+        client.force_authenticate(user=user)
+
+        post= Post(title="CodeCamp",url="https://www.django-rest-framework.org/", user=user)
+        post.save()
+        response = client.get("/"+str(post.pk)+"/comment/70/")
+        assert response.status_code == 404
+
+#Deleting comments tests
+    def test_delete_exist_comment(self):
+        client = APIClient()
+        user=CustomUser.objects.create_user("testing1", "134679//t")
+        client.force_authenticate(user=user)
+
+        post= Post(title="CodeCamp",url="https://www.django-rest-framework.org/", user=user)
+        post.save()
+        comment= Comment(comment="My Comment", user=user, post=post)
+        comment.save()
+    
+        response = client.delete("/"+str(post.pk)+"/comment/"+str(comment.pk))
+        assert response.status_code == 200
+  
+    def test_delete_unexist_comment(self):
+        client = APIClient()
+        user=CustomUser.objects.create_user("testing1", "134679//t")
+        client.force_authenticate(user=user)
+
+        post= Post(title="CodeCamp",url="https://www.django-rest-framework.org/", user=user)
+        post.save()
+        response = client.delete("/"+str(post.pk)+"/comment/70/")
+        assert response.status_code == 404
--- mysite/views.py
+++ mysite/views.py
@@ -11,21 +11,20 @@ from .helpers import get_post, get_comment
 
 
 class PostListAPIView(APIView):
-        permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
-        def get(self, request, *args, **kwargs):
-                posts = Post.objects.all()
-                serializer = PostSerializer(posts, many = True)
-                return Response(serializer.data, status = status.HTTP_200_OK)
+    def get(self, request, *args, **kwargs):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many = True)
+        return Response(serializer.data, status = status.HTTP_200_OK)
 
-        def post(self,request, *args, **kwargs):
-      
-                serializer = PostSerializer(data= request.data)
-                if serializer.is_valid():
-                        serializer.save(user_id=request.user.id)
-                        return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
-        
-                return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    def post(self,request, *args, **kwargs):
+
+        serializer = PostSerializer(data= request.data)
+        if serializer.is_valid():
+            serializer.save(user_id=request.user.id)
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
         
 class PostDetailAPIView(APIView):
 
@@ -77,25 +76,25 @@ class UserPostAPIView(APIView):
         return Response(serializer.data, status = status.HTTP_200_OK)
 
 class CommentAPIView(APIView):
-        permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
-        def get(self, request, pk, *args, **kwargs):
-                post = get_post(self, pk)
-                if post is None:
-                        return Response({'error': 'Post not found'}, status = status.HTTP_404_NOT_FOUND)
-                comments = Comment.objects.filter(post_id=self.kwargs["pk"])
-                serializer= CommentSerializer(comments, many = True)
-                return Response(serializer.data, status = status.HTTP_200_OK)
+    def get(self, request, pk, *args, **kwargs):
+        post = get_post(self, pk)
+        if post is None:
+            return Response({'error': 'Post not found'}, status = status.HTTP_404_NOT_FOUND)
+        comments = Comment.objects.filter(post_id=self.kwargs["pk"])
+        serializer= CommentSerializer(comments, many = True)
+        return Response(serializer.data, status = status.HTTP_200_OK)
 
-        def post(self, request, pk, *args, **kwargs):
-                post = get_post(self,pk)
-                if post is None:
-                        return Response({'error': 'Post not found'}, status = status.HTTP_404_NOT_FOUND)
-                serializer = CommentSerializer(data = request.data)
-                if serializer.is_valid():
-                    serializer.save(user_id=request.user.id,post_id=self.kwargs["pk"])
-                    return Response(serializer.data, status = status.HTTP_201_CREATED)
-                return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    def post(self, request, pk, *args, **kwargs):
+        post = get_post(self,pk)
+        if post is None:
+            return Response({'error': 'Post not found'}, status = status.HTTP_404_NOT_FOUND)
+        serializer = CommentSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save(user_id=request.user.id,post_id=self.kwargs["pk"])
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
 class CommentDetailAPIView(APIView):
     def get(self, requset, pk,pk2 ,*args, **kwargs):
@@ -117,25 +116,25 @@ class CommentDetailAPIView(APIView):
         return Response({"error": "You are not authorized to delete this post"}, status = status.HTTP_401_UNAUTHORIZED)    
 
 class LikeAPIView(APIView):
-        permission_classes = [permissions.IsAuthenticated]
-        def post(self, request, pk, *args, **kwargs):
-                post = get_post(self, pk)
-                if post is None:
-                    return Response({'error': 'Post not found'}, status = status.HTTP_404_NOT_FOUND)
+    permission_classes = [permissions.IsAuthenticated]
+    def post(self, request, pk, *args, **kwargs):
+        post = get_post(self, pk)
+        if post is None:
+            return Response({'error': 'Post not found'}, status = status.HTTP_404_NOT_FOUND)
 
-                like = Like.objects.filter(post=post.id, user=request.user).first()
-                if like:
-                    like.delete()
-                    post.likes_count -=1
-                    post.save()
-                    return Response({"Error": "You unliked this post"}, status=status.HTTP_200_OK)
+        like = Like.objects.filter(post=post.id, user=request.user).first()
+        if like:
+            like.delete()
+            post.likes_count -=1
+            post.save()
+            return Response({"Error": "You unliked this post"}, status=status.HTTP_200_OK)
 
-                else:
-                    like= Like.objects.create(user= request.user, post= post )
-                    like.save()
-                    post.likes_count +=1
-                    post.save()
-                    return Response({"status": "You liked this post"}, status=status.HTTP_200_OK)
+        else:
+            like= Like.objects.create(user= request.user, post= post )
+            like.save()
+            post.likes_count +=1
+            post.save()
+            return Response({"status": "You liked this post"}, status=status.HTTP_200_OK)
 
                    
    
